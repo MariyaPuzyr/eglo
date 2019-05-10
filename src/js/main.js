582d8ff2705330eb31acebeb3559101fbeeab664
MariyaPuzyr <mariyapyzur@gmail.com>
2019-05-10 15:00:48 +0300

    шапка - 1280
    шапка - 1280

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
-$(".header-top-menu__menu-item--link").click(function() {
-  $(this).parents().toggleClass("show");
+$('.menu-call').click(function () {
+  var classnow = $(this).parents().attr('class');
+  $('.header-top-menu__menu-item li').removeClass('show');
+  if(classnow != 'show')
+    $(this).parents('.header-top-menu__menu-item li').toggleClass('show');
+});
+
+/*hide select list on document click*/
+$(document).click(function (e) {
+  if (!$(e.target).closest(".menu-call").length) {
+    $('.header-top-menu__menu-item li').removeClass('show');
+  }
+  e.stopPropagation();
+});
+
+/* hide top info block */
+$('.close').click(function () {
+  $(this).parents('.top-info').hide();
 });
