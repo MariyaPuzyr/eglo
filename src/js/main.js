827cb676deb0596275111b9a96e4189084ab0580
MariyaPuzyr <mariyapyzur@gmail.com>
2019-05-20 09:40:06 +0300

    fix header menu and add responsive scripts to slider
    fix header menu and add responsive scripts to slider

--- src/js/main.js
+++ src/js/main.js
@@ -33,9 +33,33 @@ $('.close-menu').click(function () {
 /* slider */
 $('.single-item').slick({
   dots: true,
-
   infinite: true,
   slidesToShow: 1,
-  adaptiveHeight: true
+  adaptiveHeight: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
 /* slider end */
